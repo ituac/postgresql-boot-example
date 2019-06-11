9f14c1c92c46a39f5287900c0f87a375b2b80a69
ituac <ituac@qq.com>
2019-06-11 11:15:40 +0800

    init

---
+++ src/test/java/org/ituac/PostgresqlBootExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ituac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostgresqlBootExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
